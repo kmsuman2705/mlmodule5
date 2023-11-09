214b31037dd778153ae14259614a5b767b3d63bc
Suman Kumar Mishra <90380803+kmsuman2705@users.noreply.github.com>
2023-11-09 16:11:40 +0530

    Update module5.py

--- module5.py
+++ module5.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-pip install scikit-learn
-from sklearn.linear_model import LogisticRegression
-
-
-
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import joblib
 
+# Load the trained model
+model_path = 'full/path/to/Species_mind.pk1'
+mind = joblib.load(model_path)
 
-# In[23]:
-
-
-mind = joblib.load('Species_mind.pk1')
 def predict_species():
     print("Enter the sepal length, sepal width, petal length, and petal width of the flower:")
     sepal_length = float(input("Sepal Length: "))
@@ -33,11 +26,3 @@ def predict_species():
 
 # Test the function
 predict_species()
-
-
-
-# In[ ]:
-
-
-
-
